1edf859761833c6af19a10e687c7ebaa27c8c096
GBamfo <geokbamfo@gmail.com>
2023-02-16 16:08:23 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main: function block using the printf function to print out an output
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
