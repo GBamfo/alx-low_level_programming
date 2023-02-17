62c63ef643d8144e2f8bc524c802acb872a94dee
GBamfo <geokbamfo@gmail.com>
2023-02-17 11:19:11 -0800

    printing alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x += 1)
+	{
+		putchar(x);
+	}
+
+	return (0);
+}
