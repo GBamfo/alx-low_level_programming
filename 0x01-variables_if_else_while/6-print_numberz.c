d1941796ea4cf5ac28b6a49935c8d36ca9181650
GBamfo <geokbamfo@gmail.com>
2023-02-17 16:18:52 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	while (x <= 9)
+	{
+		putchar(x + '0');
+		x += 1;
+	}
+	putchar(10);
+	return (0);
+}
