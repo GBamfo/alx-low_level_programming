1fb94ae4afe0bb51d86d91892aa3771841330df7
GBamfo <geokbamfo@gmail.com>
2023-02-21 10:52:57 -0800

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		_putchar(x);
+		x++;
+	}
+	_putchar(0);
+
+	return (0);
+}
