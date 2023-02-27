3e30dae5b547f62a211f93bff1daa17504d0a7f0
GBamfo <geokbamfo@gmail.com>
2023-02-27 15:13:51 -0800

    Updating the value of a pointer

---
+++ 0x05-pointers_arrays-strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - A function that updates the value it points to to 98
+ * @n: the parameter to be updated
+ * Return: 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
