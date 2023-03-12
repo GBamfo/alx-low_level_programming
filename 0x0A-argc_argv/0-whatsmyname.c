986d5b802989dbda31362f6e80552c73892b4d21
GBamfo <geokbamfo@gmail.com>
2023-03-12 15:42:06 -0700

    a function that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,13 +3,12 @@
  *main - program that prints its name
  * @argc: the counter of commandline arguments
  * @argv: vector of pointer to strings
- * 
  * Return: 0 (success)
  */
 int main(int argc, char *argv[])
 {
-		(void) argc;
-			printf("%s\n", argv[0]);
+	(void) argc;
+	printf("%s\n", argv[0]);
 
-				return (0);
+	return (0);
 }
